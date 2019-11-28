e9b1b7c6cc056437cee81851a4c2a031e38d71b4
xdyangwei <xdgodyw@gmail.com>
2019-11-28 14:02:45 +0800

    improve the hmm model

--- hmm.py
+++ hmm.py
@@ -1,5 +1,6 @@
 from hmmlearn.hmm import GaussianHMM
 import numpy as np
+import math
 data=open("C:\\transfer_data.txt")
 data=data.readlines()
 sample=[]
@@ -11,8 +12,10 @@ for index in range(len(data)):
         sample.append(sample1)
 # for x in sample:
 #     print(x[0],x[1])
-n=8
-X=[[1.0,2.0]]
+n=3
+X=[[0.6,-0.1]]
 model=GaussianHMM(n_components=n,n_iter=100).fit(sample)
-hidden_states = model.predict(X)
-print(hidden_states)
+print(model.startprob_)
+print(model.transmat_)
+#print(model.emissionprob_)
+print(math.exp(model.score(X)))
