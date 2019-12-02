bf5c54351875045e448b9d83268e092c131f7e4c
xdyangwei <xdgodyw@gmail.com>
2019-12-02 17:35:50 +0800

    train the hmm model for a few times to select the best model

--- hmm_test.py
+++ hmm_test.py
@@ -1,11 +1,12 @@
 import numpy as np 
 from hmmlearn import hmm
+import math
 ob=np.array([1,0,0,1,1,0,0,1,1,0,1,1,0])
 ob.shape=(len(ob),1)
 model=hmm.MultinomialHMM(n_components=3)
 np.random.seed(1)
-model.fit(ob)
-print(model.emissionprob_)
-print(model.transmat_)
-print(model.predict(ob))
-print(model.predict_proba(ob)) 
+for i in range(10):
+    model.fit(ob)
+    a,b=model.decode([[1]])
+    #np.append(ob,b)
+    print(math.exp(a),b)
